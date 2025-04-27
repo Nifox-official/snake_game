d57685b4317c4b50935bcc7553326e65e5518ed1
Nifox-official <russkiy.ejik@mail.ru>
2025-04-27 13:37:24 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class GameSprite(sprite.Sprite):
         self.rect.y = y
     def reset(self):
         window.blit(self.image,(self.rect.x, self.rect.y))
+    def collidepoint(self,x,y):
+        return self.rect.collidepoint(x,y)
 
 
 class Snake(GameSprite):
@@ -88,9 +90,15 @@ head=Snake('snake.png',350,250,25,25,0)
 speed=1
 food=Food(['apple.png','arbuz.png','orange.png'], -100,-100,25,25)
 tail=Snake('tail.png', 350,275,25,25,0)
+title=GameSprite('title.png', 200,100,300,200)
+btn_play=GameSprite('btn_play.png', 250,300,200,100)
+title_lose=GameSprite('lose.png', 200,100,300,200)
 snake=[head,tail]
 food.position()
 game=True
+menu=True
+finish=True
+lose=False
 global_wait=0
 font.init()
 font1=font.SysFont('Arial',30)
@@ -100,41 +108,69 @@ while game:
     for e in event.get():
             if e.type == QUIT:
                 game = False
-
     window.fill((0,102,160))
-    head.get_direction()
-    if global_wait==0:
-        global_wait=head.wait
-        for e in range(len(snake)-1,0,-1):
-            snake[e].direction=snake[e-1].direction
-            snake[e].set_direct()
-            snake[e].rect.x=snake[e-1].rect.x
-            snake[e].rect.y=snake[e-1].rect.y
-        head.update()
-    else:
-        global_wait-=1
-    head.reset()
-    food.reset()
-    for s in snake:
-        s.reset()
-    if head.rect.colliderect(food):
-        head.eat(food)
-        s=Snake('tail.png', head.rect.x, head.rect.y,25,25,0)
-        if speed%5==0:
-            head.wait-=1
-        snake.insert(1,s)
-        if speed %2==0:
-            head.wait-=2
-        if head.wait<2:s
-            head.wait=2
+    if menu == True:
+        title.reset()
+        btn_play.reset()
+        pressed = mouse.get_pressed()
+        pos=mouse.get_pos()
+        if pressed[0]:
+            if btn_play.rect.collidepoint(pos[0],pos[1]):
+                menu=False
+                finish=False
+
+    if finish==False:      
+        head.get_direction()
+        if global_wait==0:
+            global_wait=head.wait
+            for e in range(len(snake)-1,0,-1):
+                snake[e].direction=snake[e-1].direction
+                snake[e].set_direct()
+                snake[e].rect.x=snake[e-1].rect.x
+                snake[e].rect.y=snake[e-1].rect.y
+            head.update()
+        else:
+            global_wait-=1
+        head.reset()
+        food.reset()
+        
+        for s in snake:
+            s.reset()
+        if head.rect.colliderect(food):
+            head.eat(food)
+            s=Snake('tail.png', head.rect.x, head.rect.y,25,25,0)
+            snake.insert(1,s)
+            if speed %5==0:
+                head.wait-=2
+            if head.wait<10:
+                head.wait=10
+
+        if head.rect.x <0:
+            head.rect.x=700
+
+        if head.rect.x >700:
+            head.rect.x=0
+
+        if head.rect.y <0:
+            head.rect.y=500
+
+        if head.rect.y >500:
+            head.rect.y=0
+
+        if speed>1:
+            for i in range(2,len(snake)):
+                if head.rect.colliderect(snake[i]):
+                    lose=True
+                    finish=True
+                    print(i)
+
+
+    if lose==True:
+        title_lose.reset()
 
 
 
-
-
-
-
-    score_text=font1.render('Счёт: '+str(speed),1,(0,0,0))
-    window.blit(score_text,(10,10))
+        score_text=font1.render('Счёт: '+str(speed),1,(0,0,0))
+        window.blit(score_text,(10,10))
     display.update()
     clock.tick(FPS)
